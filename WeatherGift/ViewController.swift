5b3f64e0f3816e9fa3c39b967babc13f0e4b95df
Xiaoyu Hu <hufk@bc.edu>
2019-03-18 01:22:45 -0400

    Initial Commit

---
+++ WeatherGift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherGift
+//
+//  Created by Xiaoyu Hu on 3/18/19.
+//  Copyright © 2019 Xiaoyu Hu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
